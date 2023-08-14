ce8ea466a1544635df0c97ade29c4e4834143be9
MariDantas <61691909+MariDantas@users.noreply.github.com>
2023-08-14 01:23:12 -0300

    Update countries.js

--- countries.js
+++ countries.js
@@ -1,11 +1,13 @@
 /**
  * @param {string[]} countries
  */
-export function getDropdown(countries) {
+export const getDropdown = (countries) => {
     console.log(countries);
-    let options = `<option value="">Please select</option>`;
-    countries.forEach(function(country){
-        options += `<option value="${country.toLowerCase()}">${country}</option>`
+    let countriesSelect = `<option value="">Please select</option>`
+
+    countries.forEach(country => {
+        countriesSelect += `<option value="${country.toLowerCase()}">${country}</option>`
     })
-    return options;
+
+    return countriesSelect;
 }
